2f21feb75e9c192b4d2d42366e0211b4641453b0
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-11-07 17:28:29 +0300

    added cots

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express,{Request,Response} from 'express'
 import {run} from "./repositories/db";
 import {productRouter} from "./routes/product-router";
+import cors from 'cors';
 
 
 const app = express();
 const PORT = process.env.PORT || 9000;
 
-
+app.use(cors());
 
 app.use(express.json());
 
