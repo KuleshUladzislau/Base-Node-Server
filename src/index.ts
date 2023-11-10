aecd7f472faa149e54683a381b570298507d3a75
KuleshUladzislau <kulesh_uladzislau@mail.ru>
2023-11-10 18:05:57 +0300

    fixed bug cors

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 9000;
 
 app.use(cors({
-    origin:'http://localhost:3001,http://localhost:3000',
+    origin:['http://localhost:3001','http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials:true
